a3e163387c1d9c126daf0c34ed4961aa227afd99
Alora-taken <63038255+Alora-taken@users.noreply.github.com>
2023-10-23 22:16:49 +0330

    move_logos

--- move_icons.sh
+++ move_icons.sh
@@ -2,5 +2,5 @@
 mkdir "/Users/alora/Maktab/sending projects/hw7/untitled folder/MaktabsharifMiniProject3/front/Icons"
 file="Icons.txt"
 while read -r line; do
-    mv "$line" "/Users/alora/Maktab/sending projects/hw7/untitled folder/MaktabsharifMiniProject3/front/Icons/"
+    mv "$line" "/Users/alora/Maktab/sending projects/hw7/untitled folder/MaktabsharifMiniProject3/front/Icons"
 done <$file
