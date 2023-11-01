0a5767e2c4cea00e55a21e11776491bf05826e82
yazdangheytasi <yazdangheytasi@gmail.com>
2023-11-02 00:35:13 +0330

    final edit

--- move_icons.sh
+++ move_icons.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir "/Users/alora/Maktab/sending projects/hw7/untitled folder/MaktabsharifMiniProject3/front/Icons"
+mkdir "./front/Icons"
 file="Icons.txt"
 while read -r line; do
     echo "$line"
     mv "$line" "front/Icons/"
 done <$file
-# mv "/Users/alora/Maktab/sending projects/hw7/untitled folder/MaktabsharifMiniProject3/front/images/img-1.png" "front/Icons/"
+
