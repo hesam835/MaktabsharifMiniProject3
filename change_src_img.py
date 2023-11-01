81460dc2341ddac6a062c0b5fb2b7538450eeefc
Mahdi <106872357+MahdiAlimohammadlou@users.noreply.github.com>
2023-11-01 10:40:37 -0700

    debug (null) error change_src_img

--- change_src_img.py
+++ change_src_img.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 import re
 
-directory_path = Path(r"C:\Users\Mahdi\Desktop\MaktabsharifMiniProject3\front")
+directory_path = Path(r"front")
 html_pattern = "*.html"
 
 html_file_path = directory_path.glob(html_pattern)
 img_pattern = r'<img src="images\/(.*)(-icon|-icon-1)\.png">'
 for html_file in html_file_path:
     result = None
-    with open(html_file, "r+") as f:
+    with open(html_file, "r") as f:
         content = f.read()
         result = re.sub(img_pattern, r'<img src="Icons/\1\2.png">', content)
-        f.truncate(0)
+        print(type(result))
+    with open(html_file, "w") as f:
         f.write(result)
 
 
